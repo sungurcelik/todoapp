046a0735cee2e0e0206d5e6a59b78c630236c140
Sungur Çelik <sungurcelik@Sungur-MacBook-Air.local>
2024-09-08 23:42:00 +0300

    initialize project

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import { SafeAreaView, StyleSheet, View, Text } from "react-native"
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Todo App</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+
+
+})
+
+export default App
